b6fd5e53e1f1536f7175507de6a73363c5fd0ad4
Joe Ferner <joe@fernsroth.com>
2013-12-23 17:03:59 -0500

    compile

--- build/plugins/jenkins.js
+++ build/plugins/jenkins.js
@@ -45,7 +45,7 @@ var Jenkins = (function (_super) {
 
     Jenkins.prototype.toPollResultStatus = function (state) {
         state = state.toLowerCase();
-        if (state == 'blue') {
+        if (state.indexOf('blue') == 0 || state.indexOf('disabled') == 0 || state.indexOf('aborted') == 0) {
             return PluginBase.PollResultStatus.SUCCESS;
         }
         console.error("unknown jenkins state:", state);
